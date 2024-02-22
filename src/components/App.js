f943fa8d5df8f265ad8ab36d3476d56bb755a9f4
Saikiran Thalloju <saikiranthalloju008@gmail.com>
2024-02-22 16:32:21 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,37 @@
 
-import React from "react";
-import './../styles/App.css';
+import React, { useState, useEffect } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://dummyjson.com/products");
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []); 
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Product List</h1>
+      <ul>
+        {products.map((product, index) => (
+          <li key={index}>
+            <strong>{product.name}</strong>: ${product.price}
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
